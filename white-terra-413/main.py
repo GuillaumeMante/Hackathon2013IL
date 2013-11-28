77e9e91f09aa2c025d33219e2b96ad56f7137839
GuillaumeMante <guillaume.mante@telecomnancy.net>
2013-11-28 11:33:24 +0100

    trololol

--- white-terra-413/main.py
+++ white-terra-413/main.py
@@ -18,7 +18,7 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello Fabien!<br />Oh hi Mark !')
+        self.response.write('Hello Fabien!<br />Oh hi Mark !<br /> Ololol Trololololololol')
 
 
 app = webapp2.WSGIApplication([
